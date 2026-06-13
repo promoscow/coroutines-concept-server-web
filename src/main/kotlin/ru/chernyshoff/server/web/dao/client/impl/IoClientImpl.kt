4b2fd1e2b787a3bd0e5028fd09a412aaaf78a48b
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2026-06-13 23:34:12 +0300

    app configs separate docker / local

--- src/main/kotlin/ru/chernyshoff/server/web/dao/client/impl/IoClientImpl.kt
+++ src/main/kotlin/ru/chernyshoff/server/web/dao/client/impl/IoClientImpl.kt
@@ -1,17 +1,19 @@
 package ru.chernyshoff.server.web.dao.client.impl
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import ru.chernyshoff.server.web.dao.client.IoClient
 
 @Component
 class IoClientImpl(
-    private val restTemplate: RestTemplate
+    private val restTemplate: RestTemplate,
+    @Value($$"${app.io.host}") private val ioHost: String
 ) : IoClient {
 
     override fun trace(traceId: String): String =
         restTemplate.getForObject(
-            "http://io:8021/api/io/trace/{traceId}",
+            "$ioHost/api/io/trace/{traceId}",
             String::class.java,
             traceId
         )!!
